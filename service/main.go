935c9543ad6d960a6af80a1d1da69017aee289c1
NishanthSpShetty <nishanthspshetty@gmail.com>
2020-08-17 15:51:38 +0530

    Add follower registration to leader service

--- service/main.go
+++ service/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -15,16 +21,16 @@ import (
 
 const (
 	//Register this key on consul and get  a lock on it.
-	ServiceKey  = "service/disqlogger/key/"
+	ServiceKey  = "service/disqlogger/key/master"
 	ServiceName = "distributed-quote-logger"
-	//Standard port where all follower should register with primary server.
-	PrimaryPort = 9090
 	ttlS        = "10s"
 )
 
 var isLeader = false
 var serviceId uuid.UUID
 var client *api.Client
+var HOST string
+var PORT int
 
 func signalHandler(sessionRenewalChannel chan struct{}, sessionId string) {
 	signalChannel := make(chan os.Signal)
@@ -42,16 +48,58 @@ func signalHandler(sessionRenewalChannel chan struct{}, sessionId string) {
 	os.Exit(0)
 }
 
+//connectToLeader Connect thsi service as a follower to the elected leader.
+//this will be running forever whenever there is a change in leader this routine will make sure to connect the follower to reelected service
+func connectToLeader() {
+
+	requestBody, _ := json.Marshal(map[string]interface{}{
+		"node": serviceId,
+		"host": HOST,
+		"PORT": PORT,
+	})
+	for {
+		//loop if the current node becomes the leader
+		log.Infoln("Registering this servce as a follower to the clusetr leader...")
+		//get the leader
+		kv, _, err := client.KV().Get(ServiceKey, nil)
+		if err != nil {
+			log.Errorln("Failed to get the leader information...")
+			//TODO: give it a second and loop back??
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		//check if the KV has a session
+		if !isLeader {
+			if kv != nil && kv.Session != "" {
+				//get the leader information and send a follow request.
+				leaderEndpoint := fmt.Sprintf("http://localhost:%s%s", kv.Value, "/service/api/follower/register")
+				resp, err := http.Post(leaderEndpoint, "application/json", bytes.NewBuffer(requestBody))
+				if err != nil {
+					log.Errorln("Failed to register with the leader ", err)
+					return
+				}
+				response, err := ioutil.ReadAll(resp.Body)
+				log.Infof("ConnecToLeader Response : %s\n ", string(response))
+				break
+			}
+			//send connect ping to leader
+		} else {
+			log.Infoln("Im the leader....")
+			return
+		}
+	}
+}
+
 //leaderElection Function will keep trying to aquire lock on the `ServiceKey`
-func leaderElection(sessionId string) {
+func leaderElection(port int, sessionId string) {
 	//make sure there is a leader registered on the consul all time.
 	loggedOnce := false
 	for {
 		if !isLeader {
 
 			aquired, queryDuration, err := client.KV().Acquire(&api.KVPair{
-				Key:     ServiceKey + "master",
-				Value:   []byte(fmt.Sprintf("%d", PrimaryPort)),
+				Key:     ServiceKey,
+				Value:   []byte(fmt.Sprintf("%d", port)),
 				Session: sessionId,
 			}, nil)
 
@@ -73,14 +121,20 @@ func leaderElection(sessionId string) {
 			time.Sleep(60 * time.Millisecond)
 
 		} else {
-			//if once the leader elected, stop the busy loop, will change this later if need to reaquire the lock\
+			//if once the leader elected, stop the busy loop, will change this later if need to reaquire the lock
 			return
 		}
 	}
 }
 
 func main() {
-	ch := make(chan int)
+
+	_, err := net.InterfaceAddrs()
+	HOST = "localhost" //addr[0].String()
+
+	//parse the command line args
+	flag.IntVar(&PORT, "port", 9090, "Service port")
+	flag.Parse()
 
 	serviceId = uuid.New()
 
@@ -90,7 +144,6 @@ func main() {
 	config := api.DefaultConfig()
 
 	config.Address = "localhost:8500"
-	var err error
 	client, err = api.NewClient(config)
 
 	if err != nil {
@@ -114,9 +167,14 @@ func main() {
 	log.Debugf("Consul session created, ID : %v, Aquired in :%dms  ", sessionId, queryDuration.RequestTime.Milliseconds())
 
 	//Start leader election routine
-	go leaderElection(sessionId)
+	go leaderElection(PORT, sessionId)
 	doneChan := make(chan struct{})
 	go signalHandler(doneChan, sessionId)
+	//connect to leader
+
+	//TODO: try to connect to the leader, if not found call the leader election routine to make this service as the leader,
+	//so we shouyld start the leader connection routine.
+	go connectToLeader()
 	//Create a session renewer routine.
 	go func() {
 		defer close(doneChan)
@@ -126,7 +184,18 @@ func main() {
 		}
 	}()
 
-	log.Infoln("Blocking on channel recieve")
+	log.Infof("Starting HTTP service at %s:%d \n", HOST, PORT)
+
 	//start the work.
-	ch <- 1
+	address := fmt.Sprintf("%s:%d", HOST, PORT)
+	http.HandleFunc("/service/api/follower/register", func(w http.ResponseWriter, req *http.Request) {
+		requestBody, _ := ioutil.ReadAll(req.Body)
+		log.Infof("Request recieved for follower registration %v ", string(requestBody))
+
+		fmt.Fprintf(w, "Follower registered service  %s\n", serviceId)
+	})
+	http.HandleFunc("/ping", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "PONG")
+	})
+	log.Panic(http.ListenAndServe(address, nil))
 }
