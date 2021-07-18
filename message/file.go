38c76a46ba43258ab760bbd63f28d726e45c7a5d
NishanthSpShetty <nishanthspshetty@gmail.com>
2021-07-18 22:28:15 +0530

    create fixed size accumulator buffer and reset to reuse the underlying slice provided

--- message/file.go
+++ message/file.go
@@ -15,7 +15,8 @@ import (
 
 //Number is picked from the rust program for copying large files, 8kb seems to perform well.
 //Should be benchmarked and updated accordignly if needed.
-const DEFAULT_BUFFER_SIZE = 1024 * 8
+const DEFAULT_READ_CHUNK_SIZE = 1024 * 4
+const MESSAGE_KEY_VAL_SEPERATOR = "="
 
 func getTopicDatDir(dataDir string, topic string) string {
 	if !strings.HasSuffix(dataDir, "/") {
@@ -63,7 +64,7 @@ func writeToLogFile(dataDir string, topic string, messages []Message) (int, erro
 	counter := 0
 	for _, message := range messages {
 		counter += 1
-		buf.WriteString(fmt.Sprintf("%d=%s", message.Id, message.Data))
+		buf.WriteString(fmt.Sprintf("%d%s%s", message.Id, MESSAGE_KEY_VAL_SEPERATOR, message.Data))
 	}
 	n, err := fw.Write(buf.Bytes())
 	if err != nil {
@@ -92,12 +93,31 @@ func readFromLog(dataDir, topic string, fileOffset int64) ([]Message, error) {
 	if err != nil {
 		return nil, err
 	}
-	buf := make([]byte, DEFAULT_BUFFER_SIZE)
+	buf := make([]byte, DEFAULT_READ_CHUNK_SIZE)
 	reader := bufio.NewReader(file)
 	var n int
 	var bufWriteError error
-	//should we create a buffer of fixed size?
-	buffer := new(bytes.Buffer)
+
+	//chunk size of 8kb does provide improved result, anything less than a page size worsens below num
+	//bench: Reading 1GB file
+	//using 1GB buffer size
+	//took ~255ms to 295ms
+	//using 1Mb buffer
+	//took 370ms to 690ms
+
+	//bench: Reading 100MB file
+	//using 1MB buffer size
+	//took ~50ms to ~70ms
+	//using 1Kb buffer
+	//took ~50ms to ~80ms
+
+	//Refer : https://github.com/NishanthSpShetty/bench_disk_io.go
+
+	//TODO: look into reuse of this buffer
+	buffer := bytes.NewBuffer(make([]byte, 1024*1024))
+	//calling reset will make buffer.Write reuse the underlying buffer.
+	//if not called, on write it will grow the underlying buffer causing allocation
+	buffer.Reset()
 	for {
 		n, err = reader.Read(buf)
 		if n == 0 {
@@ -125,7 +145,7 @@ func decodeRawMessage(raw []byte) []Message {
 	messages := make([]Message, 0)
 	for _, line := range strings.Split(string(raw), "\n") {
 		message := Message{}
-		splits := strings.Split(line, "=")
+		splits := strings.Split(line, MESSAGE_KEY_VAL_SEPERATOR)
 		if len(splits) != 2 {
 			continue
 		}
